081bc6429dc16175f8369d9aef3bc6a0e67222de
Ahmed El-Mahdi <agelmahdi@gmail.com>
2022-01-01 19:13:17 +0200

    [Permissions] handle location permissions

---
+++ app/src/main/java/com/agelmahdi/trackingapp/Others/TrackingUtil.kt
@@ -0,0 +1,29 @@
+package com.agelmahdi.trackingapp.Others
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import com.agelmahdi.trackingapp.Others.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import pub.devrel.easypermissions.EasyPermissions
+
+object TrackingUtil {
+    fun hasLocationPermissions(context: Context) =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+
+                )
+        }
+
+
+
+}
--- app/src/main/java/com/agelmahdi/trackingapp/UI/Fragments/RunFragment.kt
+++ app/src/main/java/com/agelmahdi/trackingapp/UI/Fragments/RunFragment.kt
@@ -1,5 +1,8 @@
 package com.agelmahdi.trackingapp.UI.Fragments
 
+import android.Manifest
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +10,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.agelmahdi.trackingapp.Others.Constants
+import com.agelmahdi.trackingapp.Others.TrackingUtil
 import com.agelmahdi.trackingapp.R
 import com.agelmahdi.trackingapp.UI.MainViewModel
 import com.agelmahdi.trackingapp.databinding.FragmentRunBinding
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment: Fragment() {
+class RunFragment : Fragment(), EasyPermissions.PermissionCallbacks {
+
     private val viewModel: MainViewModel by viewModels()
+
     private var _binding: FragmentRunBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,12 +41,66 @@ class RunFragment: Fragment() {
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+
+        checkPermissions()
+
         return view
     }
 
+    private fun checkPermissions() {
+
+        if (TrackingUtil.hasLocationPermissions(requireContext())) {
+            return
+        }
+
+        requestPermissions()
+    }
+
+    fun requestPermissions() =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app",
+                Constants.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app",
+                Constants.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+
+                )
+        }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
