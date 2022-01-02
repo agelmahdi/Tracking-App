fec11625575498eaaf367fc744c25becdec4cb1f
Ahmed El-Mahdi <agelmahdi@gmail.com>
2022-01-02 20:51:19 +0200

    [Tracking service] handle foreground service

--- app/src/main/java/com/agelmahdi/trackingapp/Sevices/TrackingService.kt
+++ app/src/main/java/com/agelmahdi/trackingapp/Sevices/TrackingService.kt
@@ -1,20 +1,42 @@
 package com.agelmahdi.trackingapp.Sevices
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import com.agelmahdi.trackingapp.Others.Constants.ACTION_PAUSE_SERVICE
+import com.agelmahdi.trackingapp.Others.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.agelmahdi.trackingapp.Others.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.agelmahdi.trackingapp.Others.Constants.ACTION_STOP_SERVICE
+import com.agelmahdi.trackingapp.Others.Constants.NOTIFICATION_CHANNEL_ID
+import com.agelmahdi.trackingapp.Others.Constants.NOTIFICATION_CHANNEL_NAME
+import com.agelmahdi.trackingapp.Others.Constants.NOTIFICATION_ID
+import com.agelmahdi.trackingapp.R
+import com.agelmahdi.trackingapp.UI.MainActivity
 import timber.log.Timber
 
-class TrackingService: LifecycleService() {
+class TrackingService : LifecycleService() {
+
+    var isServiceStarted = true
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         intent?.let {
-            when(it.action){
+            when (it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    Timber.d(ACTION_START_OR_RESUME_SERVICE)
+                    if (isServiceStarted) {
+                        startForegroundService()
+                        isServiceStarted = !isServiceStarted
+                    } else {
+                        Timber.d("Resuming service....")
+                    }
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d(ACTION_PAUSE_SERVICE)
@@ -25,5 +47,44 @@ class TrackingService: LifecycleService() {
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForegroundService() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuild = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running app")
+            .setContentText("00:00:00")
+            .setContentIntent(mainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_ID, notificationBuild.build())
+
+    }
+
+    private fun mainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 }
--- app/src/main/java/com/agelmahdi/trackingapp/UI/MainActivity.kt
+++ app/src/main/java/com/agelmahdi/trackingapp/UI/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.agelmahdi.trackingapp.UI
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.agelmahdi.trackingapp.Others.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 
 import com.agelmahdi.trackingapp.R
 import com.agelmahdi.trackingapp.databinding.ActivityMainBinding
@@ -14,6 +18,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment : NavHostFragment
+    private lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,13 +27,23 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         setupNavHostFragment()
+        navigateTrackingFragment(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateTrackingFragment(intent)
+
     }
 
     private fun setupNavHostFragment() {
-        val navHostFragment =
+        navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
+
+        navController = navHostFragment.navController
+
         binding.bottomNavigationView.setupWithNavController(navController)
+
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
@@ -37,4 +53,11 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun navigateTrackingFragment(intent: Intent?){
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){
+            navController.navigate(R.id.action_global_tracking_fragment)
+        }
+    }
+
 }
